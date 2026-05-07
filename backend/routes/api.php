bc32c7baf0e9dc1761a4e8beb0bad1e618fa38b7
Meera  Fareena <ajmalfareena2006@gmail.com>
2026-05-07 12:11:35 +0530

    Add health check endpoint to API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,3 +7,9 @@ Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
 Route::patch('/tasks/{id}', [TaskController::class, 'update']);
 Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok'
+    ]);
+});
